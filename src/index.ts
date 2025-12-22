6f90c3bafc4b3c0177b90d61b28a4bd9648e945f
dannieljl <mdjocol@gmail.com>
2025-12-22 13:06:59 -0600

    Nuevos configs

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ const start = async () => {
         await fastify.listen({ port, host });
 
         console.log(
-            `🚀 Servidor Skip-Bo v1.0.0 corriendo en ${
+            `🚀 Servidor Skip-Bo v1.0.0 run in ${
                 host === '0.0.0.0' ? 'http://localhost' : host
             }:${port}`
         );
